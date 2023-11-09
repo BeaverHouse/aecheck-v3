5b694f721e8ef031f78600e71b10791a33c2eebc
LHU <haulrest@naver.com>
2023-11-09 13:59:47 +0000

    Migrate data from other repository

--- src/constant/updates.tsx
+++ src/constant/updates.tsx
@@ -1,47 +1,82 @@
-export const pickups = [337, 338]
-export const new_manifests = [125]
-export const new_buddies = [2009, 2010]
-export const new_aligns = [255, 338]
+export const pickups = [339, 340];
+export const new_manifests = [125];
+export const new_buddies = [2009, 2010];
+export const new_aligns = [340];
 
-export const announceDate = "20231102"
+export const announceDate = "20231109";
 
-export const announceTitleKo = "사이트 테스트 중입니다."
-export const announceContentKo = <div className="announce">
-  해당 사이트는 글로벌판과 일본판의 진도가 같아질 때까지 유지되고, 이후 기존 사이트 주소로 변경될 예정입니다.<br />
-  지속적으로 데이터를 백업해 주세요.
-  <br />
-  <br />
-  오류가 있다면, 꼭 제보 부탁드립니다.
-  <br />
-  <br />
-  231102 : 여러 수정사항이 있습니다. 자세한 내용은 패치노트를 확인해 주세요.
-  <br />
-  <br />
-  <a href="https://aecheck.tistory.com/category/%EC%A0%95%EB%B3%B4%2C%EB%AC%B8%EC%9D%98" target="_blank" rel="noreferrer">사이트 관련 정보 보러가기</a>
-  <br />
-  <a href="https://aecheck.tistory.com/9" target="_blank" rel="noreferrer">오류 제보</a>
-  <br />
-  <a href="https://aecheck.tistory.com/47" target="_blank" rel="noreferrer">패치 노트</a>
-</div>
+export const announceTitleKo = "사이트 테스트 중입니다.";
+export const announceContentKo = (
+  <div className="announce">
+    해당 사이트는 글로벌판과 일본판의 진도가 같아질 때까지 유지되고, 이후 기존
+    사이트 주소로 변경될 예정입니다.
+    <br />
+    지속적으로 데이터를 백업해 주세요.
+    <br />
+    <br />
+    오류가 있다면, 꼭 제보 부탁드립니다.
+    <br />
+    <br />
+    231109 : 일판 3.4.1 버전 업데이트
+    <br />
+    <br />
+    <a
+      href="https://aecheck.tistory.com/category/%EC%A0%95%EB%B3%B4%2C%EB%AC%B8%EC%9D%98"
+      target="_blank"
+      rel="noreferrer"
+    >
+      사이트 관련 정보 보러가기
+    </a>
+    <br />
+    <a href="https://aecheck.tistory.com/9" target="_blank" rel="noreferrer">
+      오류 제보
+    </a>
+    <br />
+    <a href="https://aecheck.tistory.com/47" target="_blank" rel="noreferrer">
+      패치 노트
+    </a>
+  </div>
+);
 
-export const announceTitleEN = "Site is under Testing"
-export const announceContentEN = <div className="announce">
-  This testing site is maintained until GLOBAL version follows up JP version.<br />
-  Please backup your data while using website.
-  <br />
-  <br />
-  Please contact if you find errors.
-  <br />
-  <br />
-  231102 : Patched some utilites & errors. Details are updated on patch note.
-  <br />
-  <br />
-  <a href="https://github.com/BeaverHouse/aecheck-docs" target="_blank" rel="noreferrer">Docs</a>
-  <br />
-  <a href="https://github.com/BeaverHouse/aecheck-v3" target="_blank" rel="noreferrer">Github</a>
-  <br />
-  <a href="https://github.com/BeaverHouse/aecheck-docs/blob/main/v3_docs/patch.md#231102" target="_blank" rel="noreferrer">Patch Note</a>
-  <br />
-  <br />
-  e-mail : haulrest@gmail.com
-</div>
+export const announceTitleEN = "Site is under Testing";
+export const announceContentEN = (
+  <div className="announce">
+    This testing site is maintained until GLOBAL version follows up JP version.
+    <br />
+    Please backup your data while using website.
+    <br />
+    <br />
+    Please contact if you find errors.
+    <br />
+    <br />
+    231109 : Update JAP 3.4.1
+    <br />
+    <br />
+    <a
+      href="https://github.com/BeaverHouse/aecheck-docs"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Docs
+    </a>
+    <br />
+    <a
+      href="https://github.com/BeaverHouse/aecheck-v3"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Github
+    </a>
+    <br />
+    <a
+      href="https://github.com/BeaverHouse/aecheck-docs/blob/main/v3_docs/patch.md#231102"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Patch Note
+    </a>
+    <br />
+    <br />
+    e-mail : haulrest@gmail.com
+  </div>
+);
