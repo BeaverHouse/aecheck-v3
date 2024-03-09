9b885ffbe46a120808423857f035fb5011481cba
LHU <haulrest@gmail.com>
2024-03-09 23:44:56 +0900

    🚀 v3.5.70 Additional update (#14)

--- src/constant/updates.tsx
+++ src/constant/updates.tsx
@@ -1,24 +1,14 @@
-export const pickups = [358];
+export const pickups = [359];
 export const new_manifests = [108];
 export const new_buddies = [2011];
-export const new_aligns = [358];
+export const new_aligns = [359];
 export const version = "3.5.70";
 
-export const announceDate = "20240301";
+export const announceDate = "20240308";
 
 export const announceTitleKo = announceDate + " 업데이트";
 export const announceContentKo = (
   <div className="announce">
-    <span>1. 배포 캐릭터의 VC 글래스터를 추가했습니다.</span>
-    <br />
-    <span>2. 글래스터 체크 메뉴 이름을 명확하게 변경했습니다.</span>
-    <br />
-    <span>
-      3. 셀렉트 박스를 통해 다른 페이지로 이동할 수 있도록 변경했습니다.
-    </span>
-    <br />
-    <span>4. 기타 수정 및 조정</span>
-    <br />
     <p>Version : {version}</p>
     <br />
     <a href="https://aecheck.tistory.com/" target="_blank" rel="noreferrer">
@@ -36,14 +26,6 @@ export const announceContentKo = (
 export const announceTitleEN = announceDate + " Update";
 export const announceContentEN = (
   <div className="announce">
-    <span>1. Added VC Grasta for free characters</span>
-    <br />
-    <span>2. Changed the grasta menu name to "VC Grasta".</span>
-    <br />
-    <span>3. Added feature to navigate to other pages by select box.</span>
-    <br />
-    <span>4. Other tranlation and minor fixes</span>
-    <br />
     <p>Version : {version}</p>
     <br />
     <a
