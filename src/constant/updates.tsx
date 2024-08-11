2a1f739bbdb3cb031b74effb08cd85fba7e1a34c
HU-Lee <haulrest@gmail.com>
2024-08-11 11:37:38 +0000

    🚀 synced local 'src/constant/updates.tsx' with remote 'result/updates.tsx'

--- src/constant/updates.tsx
+++ src/constant/updates.tsx
@@ -1,15 +1,18 @@
-export const pickups = [376,377];
+export const pickups = [379];
 export const new_manifests = [149];
 export const new_buddies = [2016];
-export const new_aligns = [377];
-export const version = "3.8.20";
+export const new_aligns = [379];
+export const version = "3.8.30";
 
-export const announceDate = "20240727";
+export const announceDate = "20240811";
 
 export const announceTitleKo = announceDate + " 업데이트";
 export const announceContentKo = (
   <div className="announce">
     <p>Version : {version}</p>
+    <br />
+    <p>색맹 대응을 위한 색상 변경</p>
+    <p>이전 결과보기 기본 설정 변경</p>
     <br />
     <a href="https://aecheck.tistory.com/" target="_blank" rel="noreferrer">
       사이트 안내용 블로그
@@ -27,6 +30,9 @@ export const announceTitleEN = announceDate + " Update";
 export const announceContentEN = (
   <div className="announce">
     <p>Version : {version}</p>
+    <br />
+    <p>Change color scheme to improve colorblinded people's UX</p>
+    <p>Default Setting Change (Legacy Result)</p>
     <br />
     <a
       href="https://github.com/BeaverHouse/aecheck-v3/wiki"
