0c3e6c3eef465e0a3ac118ff549222d9da2291e4
Austin Lee <haulrest@gmail.com>
2024-08-16 00:45:59 +0900

    🚀 240816 Additional data update (#31)

--- src/constant/updates.tsx
+++ src/constant/updates.tsx
@@ -1,18 +1,17 @@
 export const pickups = [379];
-export const new_manifests = [149];
+export const new_manifests = [214];
 export const new_buddies = [2016];
 export const new_aligns = [379];
 export const version = "3.8.30";
 
-export const announceDate = "20240811";
+export const announceDate = "20240816";
 
 export const announceTitleKo = announceDate + " 업데이트";
 export const announceContentKo = (
   <div className="announce">
     <p>Version : {version}</p>
     <br />
-    <p>색맹 대응을 위한 색상 변경</p>
-    <p>이전 결과보기 기본 설정 변경</p>
+    <p>누락된 데이터 추가</p>
     <br />
     <a href="https://aecheck.tistory.com/" target="_blank" rel="noreferrer">
       사이트 안내용 블로그
@@ -31,8 +30,7 @@ export const announceContentEN = (
   <div className="announce">
     <p>Version : {version}</p>
     <br />
-    <p>Change color scheme to improve colorblinded people's UX</p>
-    <p>Default Setting Change (Legacy Result)</p>
+    <p>Add some missing data</p>
     <br />
     <a
       href="https://github.com/BeaverHouse/aecheck-v3/wiki"
